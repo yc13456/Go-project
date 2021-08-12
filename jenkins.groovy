7adba088865a53a0e1e72099e78a9883c543f28d
root <root@localhost.localdomain>
2021-08-12 14:53:30 +0800

    add jenkins.groovy

---
+++ jenkins.groovy
@@ -0,0 +1,11 @@
+node {
+    stage("cheakout"){
+        checkout([$class: 'GitSCM', branches: [[name: '**']], extensions: [], userRemoteConfigs: [[credentialsId: 'e41f358d-3d20-4c4e-99cb-8726e11819a2', url: 'git@github.com:yc13456/Go-project.git']]])
+    }
+    stage("test build"){
+        echo "test build。。。"
+    }
+    stage("resultds"){
+        echo "result"
+    }
+}
