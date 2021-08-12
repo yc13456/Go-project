1141654014f111e3ab7240bf4da3dc3c20dfa497
yc13456 <50980934+yc13456@users.noreply.github.com>
2021-08-12 21:56:24 +0800

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,4 +1,9 @@
-node {
+pipeline {
+
+   agent {
+      label 'aep-builder'
+   }
+node	{
     stage("cheakout"){
         checkout([$class: 'GitSCM', branches: [[name: '**']], extensions: [], userRemoteConfigs: [[credentialsId: 'e41f358d-3d20-4c4e-99cb-8726e11819a2', url: 'git@github.com:yc13456/Go-project.git']]])
     }
@@ -9,4 +14,5 @@ node {
         echo "result"
 	echo "result"
     }
+}
 }
