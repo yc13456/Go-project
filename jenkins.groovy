aa2d9b24a573c9a539370f76e037ee7a062f6087
yc13456 <50980934+yc13456@users.noreply.github.com>
2021-08-12 21:19:41 +0800

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -4,8 +4,6 @@ node {
     }
 	stage("build Images"){
 	docker buildx build --platform linux/arm64 -f Dockerfile -t 2267024990/hello . --push
-	echo "build images success"
-	    
     }
     stage("resultds"){
         echo "result"
