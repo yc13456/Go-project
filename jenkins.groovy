5d97b2c1ba37bc46db58d59f45c6591d4659b4c9
yc13456 <50980934+yc13456@users.noreply.github.com>
2021-08-12 21:06:31 +0800

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,7 +3,6 @@ node {
         checkout([$class: 'GitSCM', branches: [[name: '**']], extensions: [], userRemoteConfigs: [[credentialsId: 'e41f358d-3d20-4c4e-99cb-8726e11819a2', url: 'git@github.com:yc13456/Go-project.git']]])
     }
     stage("build Images"){
-        echo "build Images start。。。"
 	docker buildx build --platform linux/arm64 -f Dockerfile -t 2267024990/hello . --push
 	echo "build images success"
     }
